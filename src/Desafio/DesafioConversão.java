5875f28518916297366f1c70a08f03e869a08676
evdiniz <evcdinizjr051205@gmail.com>
2024-02-28 16:00:43 -0300

    Desafio de Conversão em Java

---
+++ src/Desafio/DesafioConversão.java
@@ -0,0 +1,28 @@
+package Desafio;
+
+import java.util.Scanner;
+
+public class DesafioConversão {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro salários: ");
+        String valor1 = entrada.next().replace(",",".");
+        System.out.print("\nDigite o segundo salários: ");
+        String valor2 = entrada.next().replace(",",".");
+        System.out.print("\nDigite o terceiro salários: ");
+        String valor3 = entrada.next().replace(",",".");
+
+        double salario1 = Double.parseDouble(valor1);
+        double salario2 = Double.parseDouble(valor2);
+        double salario3 = Double.parseDouble(valor3);
+
+        double media = (salario1 + salario2 + salario3) / 3;
+        System.out.println("A média dos salários é: " + media);
+
+
+
+        entrada.close();
+    }
+}
